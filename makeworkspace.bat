9306bb6faa7911dff3133b733f47f4b93c4041b9
MOZGIII <mike-n@narod.ru>
2015-04-21 21:05:27 +0300

    Cleaning up leftovers from previous dev process

--- makeworkspace.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set dir=%~dp0
-for %%* in (%dir%.) do set project=%%~n*
-
-rem Unix mkdir compat
-mkdir %dir%.goworkspace
-mkdir %dir%.goworkspace\src
-mklink /D %dir%.goworkspace\src\%project% ..\..
--- makeworkspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT="$(basename "$DIR")"
-
-mkdir -p "$DIR/.goworkspace/src"
-ln -sTf "../.." "$DIR/.goworkspace/src/$PROJECT"
